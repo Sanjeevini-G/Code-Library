a26622d4407f18c03c89aff39547b4f2f649d64b
Sanjeevini S. Gundagatti <83646267+Sanjeevini-G@users.noreply.github.com>
2023-04-23 13:17:22 +0530

    Create Euclidean_GCD.py

---
+++ Euclidean_GCD.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+result = gcd(a, b)
+print("The GCD of", a, "and", b, "is:", result)
