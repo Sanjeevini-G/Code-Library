c2c4ee5b2ddcf37df415699cb7f640e328c4bfd3
Sanjeevini S. Gundagatti <83646267+Sanjeevini-G@users.noreply.github.com>
2023-04-23 13:27:05 +0530

    Create Subset_sum_problem.py

---
+++ Subset_sum_problem.py
@@ -0,0 +1,16 @@
+def subsetSum(nums, target):
+    n = len(nums)
+    dp = [[False] * (target + 1) for _ in range(n + 1)]
+    for i in range(n + 1):
+        dp[i][0] = True
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            if j < nums[i - 1]:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+    return dp[n][target]
+
+nums = [2, 3, 7, 8, 10]
+target = 11
+print(subsetSum(nums, target))  # Output: True
