6702918805bf423772a092f723a0e40a6987a4d0
Sanjeevini S. Gundagatti <83646267+Sanjeevini-G@users.noreply.github.com>
2023-04-23 13:14:37 +0530

    Create Longest_increasing_subsequence.py

---
+++ Longest_increasing_subsequence.py
@@ -0,0 +1,12 @@
+def lis(arr):
+    n = len(arr)
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    return max(dp)
+
+arr = list(map(int, input("Enter the array elements: ").split()))
+length = lis(arr)
+print("The length of the longest increasing subsequence is:", length)
